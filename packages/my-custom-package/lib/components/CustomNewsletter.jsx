2c41343017e6df25a1137f04e18402160a2c59e1
jeffreywyman <jeffrey.wyman@gmail.com>
2016-06-21 13:46:23 -0400

    Migration to Nova, updates for CSS, formatting
    Now using mupX for deployment, with some errors (mostly warnings), and
    Telescope Nova. CSS has been updated with a few adjustments to color
    palette. Simplified UX. Social accounts still not working correctly.

--- packages/my-custom-package/lib/components/CustomNewsletter.jsx
+++ packages/my-custom-package/lib/components/CustomNewsletter.jsx
@@ -1,7 +1,7 @@
 /*
-The original Newsletter components is defined using an 
+The original Newsletter components is defined using an
 ES6 class, so we use the "class foo extends bar" syntax
-to extend it. This way, we can simply redefine the 
+to extend it. This way, we can simply redefine the
 render method to change it, while preserving
 all of the class's other methods (other render
 functions, event handlers, etc.).
@@ -17,7 +17,7 @@ class CustomNewsletter extends Telescope.components.Newsletter {
     return this.state.showBanner
       ? (
         <div className="newsletter">
-          <h4 className="newsletter-tagline">✉️<FormattedMessage id="newsletter.subscribe_prompt"/>✉️</h4>
+          <h4 className="newsletter-tagline"><FormattedMessage id="newsletter.subscribe_prompt"/> ✉️</h4>
           {this.context.currentUser ? this.renderButton() : this.renderForm()}
           <a onClick={this.dismissBanner} className="newsletter-close"><Telescope.components.Icon name="close"/></a>
         </div>
--- packages/my-custom-package/lib/components/CustomPostsItem.jsx
+++ packages/my-custom-package/lib/components/CustomPostsItem.jsx
@@ -7,11 +7,33 @@ import { Link } from 'react-router';
 
 class CustomPostsItem extends Component {
 
+renderCategories() {
+  return this.props.post.categoriesArray ? <Telescope.components.PostsCategories post={this.props.post} /> : "";
+}
+
+renderCommenters() {
+  return this.props.post.commentersArray ? <Telescope.components.PostsCommenters post={this.props.post}/> : "";
+}
+
+renderActions() {
+
+  const component = (
+    <ModalTrigger title="Edit Post" component={<a className="posts-action-edit"><FormattedMessage id="posts.edit"/></a>}>
+      <Telescope.components.PostsEditForm post={this.props.post}/>
+    </ModalTrigger>
+  );
+
+  return (
+    <div className="post-actions">
+      {Users.can.edit(this.context.currentUser, this.props.post) ? component : ""}
+    </div>
+  )
+}
   render() {
 
     const post = this.props.post;
 
-    let postClass = "posts-item"; 
+    let postClass = "posts-item";
     if (post.sticky) postClass += " posts-sticky";
 
     // ⭐ custom code starts here ⭐
@@ -22,22 +44,24 @@ class CustomPostsItem extends Component {
 
     return (
       <div className={postClass}>
-        
+
         <div className="posts-item-vote">
           <Telescope.components.Vote post={post} currentUser={this.context.currentUser}/>
         </div>
-        
+
         {post.thumbnailUrl ? <Telescope.components.PostsThumbnail post={post}/> : null}
 
         <div className="posts-item-content">
-          
+
           <h3 className="posts-item-title">
-            <Link to={Posts.getLink(post)} className="posts-item-title-link" target={Posts.getLinkTarget(post)}>
+            <Link to={Posts.getPageUrl(post)} className="posts-item-title-link">
               {post.title}
             </Link>
             {this.renderCategories()}
           </h3>
-          
+
+
+
           <div className="posts-item-meta">
             {post.user? <div className="posts-item-user"><Telescope.components.UsersAvatar user={post.user} size="small"/><Telescope.components.UsersName user={post.user}/></div> : null}
             <div className="posts-item-date"><FormattedRelative value={post.postedAt}/></div>
@@ -53,13 +77,18 @@ class CustomPostsItem extends Component {
         </div>
 
         {this.renderCommenters()}
-        
-      
+
+        <div className="posts-item-import">
+        <Link to={Posts.getLink(post)} className="post-import" target="_blank">
+          Import
+        </Link>
+        </div>
+
       </div>
     )
   }
 };
-  
+
 CustomPostsItem.propTypes = {
   post: React.PropTypes.object.isRequired
 }
@@ -68,4 +97,5 @@ CustomPostsItem.contextTypes = {
   currentUser: React.PropTypes.object
 };
 
+module.exports = CustomPostsItem;
 export default CustomPostsItem;
